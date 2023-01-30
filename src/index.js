0ec1ef41fd8229587c4dc574e91c742c550801eb
Сидорович Сергей <D:\The Bat!>
2023-01-30 16:28:57 +0300

    solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.map((i) => i.join(""));
+  
+  for (let i = 0; i < brackets.length; i++) {
+    if (str.includes(brackets[i])) {
+      str = str.replaceAll(brackets[i], "");
+      i = -1;
+    }
+  }
+  return str.length === 0 ? true : false;
 }
